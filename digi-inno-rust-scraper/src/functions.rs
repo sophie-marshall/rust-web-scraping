704b52941d9a8911282f5d8f4687515a2b158ee1
srmarshall-pbs <srmarshall@pbs.org>
2024-01-14 13:56:00 -0500

    rate limit error fix, revamp storage types

--- digi-inno-rust-scraper/src/functions.rs
+++ digi-inno-rust-scraper/src/functions.rs
@@ -1,43 +1,70 @@
+use std::error::Error;
+use reqwest::blocking::Client;
 use scraper::{Html, Selector};
 use regex::Regex;
 use spider::website::Website;
 use spider::tokio;
 
+// function to crawl a webpage for associated links 
 #[tokio::main]
 pub async fn get_links(url: &str) -> Result<Vec<String>, Box<dyn std::error::Error>> {
-    // Instantiate link vector
+    // instantiate link vector
     let mut links: Vec<String> = Vec::new();
-
+    // instantiate website object for crawling
     let mut website = Website::new(url);
     website.crawl_smart().await;
-
+    // execute smart crawl 
     for link in website.get_links() {
         links.push(link.as_ref().to_string());
     }
-
+    // return link provided crawl worked
     Ok(links)
 }
 
-// function to get text from a url
+// function to grab paragraph content form a webpage
 pub fn get_content(url: &str) -> String {
+    // initiate reqwest client to handle behavior
+    let client = Client::builder()
+                            .redirect(reqwest::redirect::Policy::limited(5))
+                            .build()
+                            .unwrap();
+    // make request 
+    let response = client.get(url).send();
+    // check and handle response types
+    match response {
+        Ok(response) => {
+            // if request okay parse and return content
+            let html_content = response.text().unwrap();
+            let document = Html::parse_document(&html_content);
 
-    let response = reqwest::blocking::get(url);
-    let html_content = response.unwrap().text().unwrap();
-    let document = Html::parse_document(&html_content);
+            let selector = Selector::parse("p").unwrap();
+            let regex = Regex::new(r"[ \t\n]{2,}").unwrap();
 
-    let selector = Selector::parse("p").unwrap();
-    let regex = Regex::new(r"[ \t\n]{2,}").unwrap();
+            let mut content = String::new();
 
-    let mut content = String::new();
-
-    for element in document.select(&selector) {
-
-        let raw_text = element.text().collect::<String>();
-        let clean_text = regex.replace_all(&raw_text, " ");
-
-        content.push_str(&clean_text);
+            for element in document.select(&selector) {
+                let raw_text = element.text().collect::<String>();
+                let clean_text = regex.replace_all(&raw_text, " ");
+                content.push_str(&clean_text);
+            }
+            // return content if things worked
+            content
+        }
+        // if error, return error types
+        Err(err) => {
+            // If there was an error, check if it's a TooManyRedirects error
+            if let Some(reqwest_error) = err.source() {
+                if reqwest_error.is::<reqwest::Error>() {
+                    // Handle the redirect error by returning a custom message
+                    "redirect limit reachted".to_string()
+                } else {
+                    // If it's not a redirect error, return the original error
+                    format!("Error: {}", err)
+                }
+            } else {
+                // If there's no source, return the original error
+                format!("Error: {}", err)
+            }
+        }
     }
-
-    // return content
-    content
 }
--- digi-inno-rust-scraper/src/main.rs
+++ digi-inno-rust-scraper/src/main.rs
@@ -1,43 +1,58 @@
 mod functions;
 use functions::{get_content, get_links};
+use spider::case_insensitive_string::serde::Deserialize;
 use std::thread::sleep;
 use std::time::Duration;
-use std::collections::HashMap;
+use std::fs::File;
+use serde::Serialize;
+use csv::Writer;
+
+// define struct for website data 
+#[derive(Debug, Serialize, Deserialize)]
+struct CrawledData {
+    link: String,
+    content: String,
+}
 
 fn main() {
-
     // define storage
-    let mut hashmap: HashMap<String, String> = HashMap::new(); // SM: Revisit, is this the right struct?
-    
+    let mut crawled_data: Vec<CrawledData> = Vec::new();
     // define base url to crawl 
     let base_url = "https://help.pbs.org/";
-
     // crawl base url for associated links
     let mut link_queue = get_links(&base_url).unwrap();
-
+    // break after first crawl
     sleep(Duration::from_secs(30));
-
+    // instantiate counter for breaking logic latter
     let mut counter = 0;
-
-    // as long as queue is filled
+    // process links until queue is empty
     while let Some(link) = link_queue.pop() {
-
-        // implement break if needed 
-        if counter % 10 == 0 && counter > 0 {
-            println!("------------ BREAK ({} Links Remaining) ------------", link_queue.len());
+        // throttle requests to comply with site limits
+        if counter % 5 == 0 && counter > 0 {
+            println!("------------ BREAK: {} Links Remaining ------------", link_queue.len());
             sleep(Duration::from_secs(10));
         }
-
         // fetch content 
         let content = get_content(&link);
-
-        // push to hash
-        hashmap.insert(link.clone(), content);
-
+        // if still hitting rate limit append link back to queue
+        if content.is_empty() {
+            println!("Implementing site specific break: {}", &link);
+            sleep(Duration::from_secs(30));
+        } else {
+            // create CrawledData instance + add to vec
+            let data = CrawledData{ link, content };
+            crawled_data.push(data);
+        }
+        // increment for tracking
         counter += 1
     }
-
-    for value in hashmap.values() {
-        println!("Value: {}", value);
+    // define filepath
+    let filepath = "/Users/srmarshall/Desktop/text_4_retry_logic.csv";
+    // create file
+    let file = File::create(filepath).expect("could not create file");
+    // write to csv 
+    let mut csv_writer = Writer::from_writer(file);
+    for entry in crawled_data {
+        csv_writer.serialize(entry).expect("Failed to write CSV")
     }
 }
