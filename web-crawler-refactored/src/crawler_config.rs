e8ed564e9b1b87c24978a77afbbcd730eee93ef8
srmarshall-pbs <srmarshall@pbs.org>
2024-01-17 17:06:08 -0500

    adjust comments

--- web-crawler-refactored/src/crawler_config.rs
+++ web-crawler-refactored/src/crawler_config.rs
@@ -10,13 +10,13 @@ pub fn configure_crawler(url: &str) -> Website {
         .with_subdomains(false) 
         .with_tld(false) 
         .with_delay(5000) 
-        .with_request_timeout(None) // default value == 15000 ms
+        .with_request_timeout(None)
         .with_http2_prior_knowledge(false)
         .with_user_agent(Some("innovation team scraping experiment bot v1.0").into())
         .with_on_link_find_callback(Some(|link, html| {
             println!("Link: {}", link.inner());
             (link, html)
-        })) // SM: not sure how this is used yet -- seems helpful to grab the link and the html from each link
+        })) // SM: prints link on each page iteation, useful for progress tracking
         .with_headers(None)
         .with_blacklist_url(None) 
         .with_proxies(None); // SM: may be worth setting up later to help distribute traffic?
